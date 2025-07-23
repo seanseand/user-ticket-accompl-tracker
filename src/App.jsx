5423d8c62d6c024e70ebd14c599d0a8c01d7ab7e
aiyan <aiyan.tuala@gmail.com>
2025-07-23 13:37:14 +0800

    feat: added a functioning ticket history button

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
 import ModalEmail from "./ModalEmail";
 import SubmitAlert from "./SubmitAlert";
+import ModalSubmittedList from "./ModalSubmittedList";
 
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [Issubmit_alertOpen, setIsSubmit_alertOpen] = useState(false);
+  const [isSubmittedListOpen, setIsSubmittedListOpen] = useState(false);
+  const [isHistoryOpen, setIsHistoryOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
-
 
   const submitTicket = (data) => {
     console.log("Ticket Submitted", data)
     setIsModalOpen(false)
     setIsSubmit_alertOpen(true);
+    setTimeout(() => {
+      setIsSubmitAlertOpen(false);
+      setIsSubmittedListOpen(true);
+    }, 1500)
   };
 return(
  <div className="app">
       <nav className="nav">
         {/* <div className="logo">Ticket System</div> */}
         <div className="about">About</div>
-        <div className="submitted-ticket">Submitted Ticket</div>
+        {/* <div className="submitted-ticket">Submitted Ticket</div> */}
       </nav>
 
       <div className="center-content">
@@ -29,7 +35,7 @@ return(
         <button className="create-button" onClick={openModal}>
           Create a Ticket
         </button>
-        <button className="btnhistory"  >
+        <button className="btnhistory" onClick={() => setIsHistoryOpen(true)} >
           Ticket History
         </button>
       </div>
@@ -40,6 +46,14 @@ return(
 
       {Issubmit_alertOpen && (
         <SubmitAlert onClose={() => setIsSubmit_alertOpen(false)}/>
+      )}
+
+      {isHistoryOpen && (
+      <ModalSubmittedList onClose={() => setIsHistoryOpen(false)} />
+      )}
+
+      {isSubmittedListOpen && (
+        <ModalSubmittedList onClose={() => setIsSubmittedListOpen(false)} />
       )}
     </div>
   );
--- src/ModalHistory.jsx
+++
@@ -1,8 +0,0 @@
-
-
-function ModalHistory({OnClose}){
-    return(
-        
-    );
-}
-export default ModalHistory
