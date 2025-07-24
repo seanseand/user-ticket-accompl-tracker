e07526ac37894c981c6a80a38958cf23e0876f45
aiyan <aiyan.tuala@gmail.com>
2025-07-24 12:50:00 +0800

    feat: added admin page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import Home from "./components/pageSetup/Home";
 import Login from "./components/pageSetup/Login"
 import RegisterPage from "./components/pageSetup/CreateAccountForm";
+import Admin from "./components/pageSetup/Admin";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/SignUp" element={<RegisterPage />} />
+      <Route path="/Admin" element={<Admin />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,13 @@ import React from "react";
 import Login from "./components/pageSetup/Login";
 import Home from "./components/pageSetup/Home";
 import RegisterPage from './components/pageSetup/CreateAccountForm.jsx'
+import Admin from './components/pageSetup/Admin.jsx'
 
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/home", element: <Home /> },
   { path: "/RegisterPage", element: <RegisterPage /> },
+  { path: "/Admin", element: <Admin /> },
 ]);
 
 createRoot(document.getElementById('root')).render(
