a4ca9919eb818f03084c7aefa76301674d9444c3
Sean <sdaoctavo@gmail.com>
2025-07-27 03:12:17 +0800

    feat: update Sidebar component with new icons and styles; refactor AccomplishmentLog layout; add Inter font support

--- src/components/page/AccomplishmentLog.jsx
+++ src/components/page/AccomplishmentLog.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Sidebar from '../layout/Sidebar'
-
+import React from "react";
+import Sidebar from "../layout/Sidebar";
 
 const AccomplishmentLog = () => {
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col h-screen">
       <nav className="nav">
         <div className="about">About</div>
       </nav>
-      <Sidebar />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 bg-[#F0F3F8]"></main>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AccomplishmentLog
+export default AccomplishmentLog;
