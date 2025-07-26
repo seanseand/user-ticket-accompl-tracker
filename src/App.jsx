4dc22199b7c939b188b5fb7cbff208b24733e6cf
Sean <sdaoctavo@gmail.com>
2025-07-27 03:38:11 +0800

    feat: refactor routing in main.jsx and simplify App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-import Home from "./components/page/Home";
-import Login from "./components/page/Login"
-import RegisterPage from "./components/page/CreateAccountForm";
-import Admin from "./components/page/Admin";
-import AccomplishmentLog from "./components/page/AccomplishmentLog";
 
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/SignUp" element={<RegisterPage />} />
-      <Route path="/Admin" element={<Admin />} />
-      <Route path="/AccomplishmentLog" element={<AccomplishmentLog />} />
-    </Routes>
-  );
+
+function App({ children }) {
+  return <>{children}</>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./styles/index.css";
 import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Login from "./components/page/Login.jsx";
 import Home from "./components/page/Home.jsx";
-import RegisterPage from './components/page/CreateAccountForm.jsx'
-import Admin from './components/page/Admin.jsx'
-import AccomplishmentLog from './components/page/AccomplishmentLog.jsx';
+import RegisterPage from "./components/page/CreateAccountForm.jsx";
+import Admin from "./components/page/Admin.jsx";
+import AccomplishmentLog from "./components/page/AccomplishmentLog/AccomplishmentLogLayout.jsx";
+import LogTime from "./components/page/AccomplishmentLog/LogTime.jsx";
+import AccomplishmentLogPage from "./components/page/AccomplishmentLog/AccomplishmentLogPage.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/home", element: <Home /> },
   { path: "/RegisterPage", element: <RegisterPage /> },
   { path: "/Admin", element: <Admin /> },
-  { path: "/AccomplishmentLog", element: <AccomplishmentLog /> },
+  {
+    path: "/AccomplishmentLog",
+    element: <AccomplishmentLog />,
+    children: [
+      { path: "log-time", element: <LogTime /> },
+      { path: "accomplishment-logs", element: <AccomplishmentLogPage /> },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
