cb200f4208b4f1410d61a4d43fc96d2f2ac92e37
Marven <luis.marvenjoffre@gmail.com>
2025-07-28 17:36:06 +0800

    + Made the page fixed and not scrollable

--- src/components/page/AccomplishmentLog/AccomplishmentLogLayout.jsx
+++ src/components/page/AccomplishmentLog/AccomplishmentLogLayout.jsx
@@ -4,13 +4,13 @@ import { Outlet } from "react-router";
 
 const AccomplishmentLog = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <nav className="nav">
+    <div className="flex flex-col min-h-screen">
+      <nav className="nav fixed top-0 w-full z-50 bg-white">
         <div className="about">About</div>
       </nav>
-      <div className="flex flex-1">
+      <div className="flex pt-[48px]"> {/* Adjust pt value to match your nav height */}
         <Sidebar />
-        <main className="flex-1 bg-[#F0F3F8]">
+        <main className="flex-1 ml-64 bg-[#F0F3F8] min-h-screen overflow-y-auto">
           <Outlet />
         </main>
       </div>
